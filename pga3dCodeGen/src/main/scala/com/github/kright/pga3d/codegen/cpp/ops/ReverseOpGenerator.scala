a18bf9004b2c503122e971122159c65583dfb94c
Igor Slobodskov <igor.slobodskov@phystech.edu>
2026-01-04 13:43:18 +0100

    cpp: refactor reversed() and antiReversed()

--- pga3dCodeGen/src/main/scala/com/github/kright/pga3d/codegen/cpp/ops/ReverseOpGenerator.scala
+++ pga3dCodeGen/src/main/scala/com/github/kright/pga3d/codegen/cpp/ops/ReverseOpGenerator.scala
@@ -1,9 +1,17 @@
 package com.github.kright.pga3d.codegen.cpp.ops
 
+import com.github.kright.ga.MultiVector
 import com.github.kright.pga3d.codegen.common.FileContent
 import com.github.kright.pga3d.codegen.cpp.{CppCodeBuilder, CppCodeGenerator, CppSubclass, CppSubclasses, Pga3dCodeGenCpp, StructBodyPart}
+import com.github.kright.symbolic.Sym
 
-class ReverseOpGenerator extends CppCodeGenerator {
+class ReverseOpGenerator extends ReverseOrAntiReverseOpsGenerator("reversed", _.reverse)
+
+class AntiReverseOpGenerator extends ReverseOrAntiReverseOpsGenerator("antiReversed", _.antiReverse)
+
+private class ReverseOrAntiReverseOpsGenerator(name: String,
+                                               operation: MultiVector[Sym] => MultiVector[Sym]) extends CppCodeGenerator {
+
   override def generateFiles(codeGen: Pga3dCodeGenCpp): Seq[FileContent] = {
     val code = CppCodeBuilder()
 
@@ -11,21 +19,27 @@ class ReverseOpGenerator extends CppCodeGenerator {
 
     code.namespace(codeGen.namespace) {
       for (cls <- CppSubclasses.all if cls.shouldBeGenerated) {
-        val result = cls.self.reverse
+        val result = operation(cls.makeSymbolic("a"))
         val target = CppSubclasses.findMatchingClass(result)
         if (target != CppSubclasses.zeroCls) {
-          code(s"constexpr ${target.name} ${cls.name}::reversed() const noexcept { return ${target.makeBracesInit(result)}; }")
+          code(s"constexpr ${target.name} $name(const ${cls.name}& a) noexcept { return ${target.makeBracesInit(result, multiline = true)}; }")
+          code(s"constexpr ${target.name} ${cls.name}::$name() const noexcept { return pga3d::${name}(*this); }")
+          code("")
         }
       }
     }
 
-    Seq(FileContent(codeGen.directory.resolve("opsReverse.h"), code.toString))
+    Seq(FileContent(codeGen.directory.resolve(s"ops${name.capitalize}.h"), code.toString))
   }
 
   override def generateStructBody(cls: CppSubclass): Seq[StructBodyPart] = {
-    val result = cls.self.reverse
+    val result = operation(cls.self)
     val target = CppSubclasses.findMatchingClass(result)
-    if (target == CppSubclasses.zeroCls) Seq() 
-    else structBodyPart(s"[[nodiscard]] constexpr ${target.name} reversed() const noexcept;")
+    val code =
+      if (target == CppSubclasses.zeroCls) ""
+      else s"[[nodiscard]] constexpr ${target.name} $name() const noexcept;"
+
+    structBodyPart(code)
   }
 }
+
